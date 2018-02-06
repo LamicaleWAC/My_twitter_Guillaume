12fc548eccbb127c0bbe1bfb26bc15e378385e40
LamicaleWAC <36199834+LamicaleWAC@users.noreply.github.com>
2018-02-06 16:40:41 +0100

    Revert "Add files via upload"
    This reverts commit 2c250989adabe95de1a70b9c70129a643c453de4.

--- Greg/password/password.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function hash_password($password, $salt)
-{
-    $hash = hash_hmac('ripemd160', $password, $salt);
-    return $hash;
-}
--- Greg/private_msg/new_msg.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$new_msg = $_POST['new_msg'];
-$db = new PDO('mysql:host=localhost;dbname=test_msg', 'root', '');
-$sql = ('INSERT INTO msg VALUES (null, ?, Now(), 1, ?)');
-session_start();
-$id = $_SESSION['id_msg'];
-$db->prepare($sql)->execute([$_POST['new_msg'], $_SESSION['id_msg']]);
-header("Location: private_msg.php?id=$id");
